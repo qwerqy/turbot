9cb943e2e6e7c59d878b9c9795b7d585d89be077
Amin Roslan <amnrsln@gmail.com>
2019-10-22 11:53:58 +0800

    test 21

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo "Start deploy"
-cd ~/app/bot
+echo Start deploy
+echo "export NODE_ENV=production" >> $BASH_ENV
+echo "export AWS_APPSYNC_APIKEY=$AWS_APPSYNC_APIKEY" >> $BASH_ENV
+echo "export AWS_APPSYNC_GRAPHQLENDPOINT=$AWS_APPSYNC_GRAPHQLENDPOINT" >> $BASH_ENV
+echo "export AWS_APPSYNC_REGION=$AWS_APPSYNC_REGION" >> $BASH_ENV
+echo "export DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN" >> $BASH_ENV
+echo "export TENOR_API_KEY=$TENOR_API_KEY" >> $BASH_ENV 
+printenv
+cd ~/app/bot/tehbot
+echo git pull
+git checkout develop
 git pull
-npm i
+echo install dependencies
+npm install
+echo compile typescript
 npm run tsc 
-pm2 stop .build/main.js
+echo start pm2
+pm2 stop main
 pm2 start .build/main.js
-echo "Deploy end"
+echo Deploy end
