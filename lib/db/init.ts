92230d0a6740ad8939ca46ab8308a15b52d4562d
Amin Roslan <amnrsln@gmail.com>
2019-10-24 00:59:28 +0800

    TODOS

--- lib/db/init.ts
+++ lib/db/init.ts
@@ -1,10 +1,18 @@
+import { isDevMode } from "./../utilities";
 import { MongoClient, Collection } from "mongodb";
 
 export let guilds: Collection;
 
+//TODO: SET ROLE ONLY ADMIN CAN ACCESS THIS. CREATE A MIDDLEWARE
 (async () => {
   // Connection URL
-  const url = process.env.MONGODB_STRING || "mongodb://localhost:27018";
+  const url = !isDevMode
+    ? process.env.MONGODB_STRING
+    : "mongodb://localhost:27018";
+
+  if (!url) {
+    throw new Error("MongoDB URL not set!");
+  }
 
   const client = new MongoClient(url, { useNewUrlParser: true });
 
--- lib/utilities.ts
+++ lib/utilities.ts
@@ -1,3 +1,7 @@
 export const getRandom = max => {
-  return Math.floor(Math.random() * Math.floor(max))
-}
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+export const isDevMode = !["staging", "production"].includes(
+  process.env.NODE_ENV
+);
