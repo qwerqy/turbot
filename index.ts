77d21f7c87e4d8ed3a8c6d9076838a382a76ac91
Amin Roslan <amnrsln@gmail.com>
2019-10-28 00:13:06 +0800

    bro

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import ws from "ws";
 import * as dotenv from "dotenv";
 import * as Sentry from "@sentry/node";
 // @ts-ignore
-global.WebSocket = ws;
+global.WebSocket = require("ws");
 dotenv.config();
 Sentry.init({
   dsn: "https://dbbae63d48f64902b049b539d2441a46@sentry.io/1796824",
