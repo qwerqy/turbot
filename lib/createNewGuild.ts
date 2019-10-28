c6fa67118d6078d9bba464b74ede473cd30223cd
Amin Roslan <amnrsln@gmail.com>
2019-10-28 21:42:01 +0800

    Added plugins object

--- lib/createNewGuild.ts
+++ lib/createNewGuild.ts
@@ -1,7 +1,14 @@
 import * as Discord from "discord.js";
 import * as Amplify from "aws-amplify";
 import * as Sentry from "@sentry/node";
-import { createGuildBot, createCommand, listGuildBots } from "./graphql";
+import {
+  createGuildBot,
+  createCommand,
+  listGuildBots,
+  createPlugin,
+  createPluginCommand,
+  createPluginSetting,
+} from "./graphql";
 
 async function createNewGuild(guild: Discord.Guild, globalPrefix: GPObject) {
   try {
@@ -17,67 +24,10 @@ async function createNewGuild(guild: Discord.Guild, globalPrefix: GPObject) {
     };
 
     const newPlugin = {
-      id: "music",
+      name: "music",
       pluginGuildBotId: guild.id,
     };
 
-    const newPluginCommands = [
-      {
-        pluginCommandPluginId: "music",
-        cmd: "play",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "search",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "skip",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "queue",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "pause",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "resume",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "volume",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "leave",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "clearqueue",
-      },
-      {
-        pluginCommandPluginId: "music",
-        cmd: "np",
-      },
-    ];
-
-    const newPluginSettings = [
-      {
-        name: "anyoneCanSkip",
-        enabled: true,
-      },
-      {
-        name: "anyoneCanAdjust",
-        enabled: true,
-      },
-      {
-        name: "ownerOverMember",
-        enabled: true,
-      },
-    ];
     // Add new guild
     await Amplify.API.graphql(
       Amplify.graphqlOperation(createGuildBot, { input: newGuildBot })
@@ -86,6 +36,107 @@ async function createNewGuild(guild: Discord.Guild, globalPrefix: GPObject) {
     await Amplify.API.graphql(
       Amplify.graphqlOperation(createCommand, { input: newCommand })
     );
+
+    const { data: newPluginData } = await Amplify.API.graphql(
+      Amplify.graphqlOperation(createPlugin, { input: newPlugin })
+    );
+
+    const newPluginCommandsList = [
+      {
+        name: "play",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "play",
+      },
+      {
+        name: "search",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "search",
+      },
+      {
+        name: "loop",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "loop",
+      },
+      {
+        name: "skip",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "skip",
+      },
+      {
+        name: "queue",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "queue",
+      },
+      {
+        name: "pause",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "pause",
+      },
+      {
+        name: "resume",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "resume",
+      },
+      {
+        name: "volume",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "volume",
+      },
+      {
+        name: "leave",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "leave",
+      },
+      {
+        name: "clearqueue",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "clearqueue",
+      },
+      {
+        name: "np",
+        pluginCommandPluginId: newPluginData.createPlugin.id,
+        cmd: "np",
+      },
+    ];
+
+    const newPluginSettingsList = [
+      {
+        pluginSettingPluginId: newPluginData.createPlugin.id,
+        name: "anyoneCanSkip",
+        enabled: true,
+      },
+      {
+        pluginSettingPluginId: newPluginData.createPlugin.id,
+        name: "anyoneCanAdjust",
+        enabled: true,
+      },
+      {
+        pluginSettingPluginId: newPluginData.createPlugin.id,
+        name: "ownerOverMember",
+        enabled: true,
+      },
+    ];
+
+    await Promise.all(
+      newPluginCommandsList.map(async newPluginCommand => {
+        await Amplify.API.graphql(
+          Amplify.graphqlOperation(createPluginCommand, {
+            input: newPluginCommand,
+          })
+        );
+      })
+    );
+
+    await Promise.all(
+      newPluginSettingsList.map(async newPluginSetting => {
+        await Amplify.API.graphql(
+          Amplify.graphqlOperation(createPluginSetting, {
+            input: newPluginSetting,
+          })
+        );
+      })
+    );
+
     const { data } = await Amplify.API.graphql(
       Amplify.graphqlOperation(listGuildBots)
     );
